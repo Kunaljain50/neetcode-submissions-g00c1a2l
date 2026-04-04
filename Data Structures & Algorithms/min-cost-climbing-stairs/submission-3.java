1ac3d22342fb58d0a239ef4c13cd828b1c5d6a9d
Kunal Jain <44358768+Kunaljain50@users.noreply.github.com>
2026-04-04 18:59:09 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        return Math.min(memo(cost, n-1, dp), memo(cost, n-2, dp));
+    }
+
+    public int memo(int[] cost, int n, int[] dp) {
+        if(n < 0) return 0;
+        if(n == 0 || n == 1) return cost[n];
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = cost[n] + Math.min(
+            memo(cost, n-1, dp),
+            memo(cost, n-2, dp)
+        );
+    }
+}
