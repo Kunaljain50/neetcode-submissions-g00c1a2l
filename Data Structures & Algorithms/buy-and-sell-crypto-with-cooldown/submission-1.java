78543dd2ce91ced8b74a30204d0bafdb55010e09
Kunal Jain <44358768+Kunaljain50@users.noreply.github.com>
2026-04-07 09:43:00 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        return memo(prices, n, 0, 0, dp);
+    }
+
+    public static int memo(int[] prices, int n, int i, int buy, int[][] dp) {
+
+        if(i >= n) return 0;
+        if (dp[i][buy] != -1) return dp[i][buy];
+
+        if(buy == 0) {
+            return dp[i][buy] = Math.max(0 + memo(prices, n, i+1, 0, dp),
+             -1 * prices[i] + memo(prices, n, i+1, 1, dp));
+        } else {
+            return dp[i][buy] = Math.max(0 + memo(prices, n, i+1, 1, dp),
+             prices[i] + memo(prices, n, i+2, 0, dp));
+        }
+    }
+}
