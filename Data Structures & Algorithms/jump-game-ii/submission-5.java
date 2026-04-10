463073ac593cb2a801186ce42414c0f5d7051269
Kunal Jain <44358768+Kunaljain50@users.noreply.github.com>
2026-04-10 08:34:34 +0530

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int end = 0;
+        int farthest = 0;
+        int jump = -1;
+
+        for(int i = 0; i<n; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if(i == end) {
+                end = farthest;
+                jump++;
+            }
+        }
+
+        return jump;
+    }
+
+    public static int dfs(int idx, int n, int[] nums, int[] dp) {
+        if(idx >= n) {
+            return 0;
+        }
+
+        if(dp[idx] != -1) return dp[idx];
+
+        int end = idx + nums[idx];
+        int jump = 1000000;
+        for(int i = idx+1; i<=end; i++) {
+            jump = Math.min(jump, 1 + dfs(i, n, nums, dp));
+        }
+
+        return dp[idx] = jump;
+    }
+}
